58cae1f2e628a7ce65331a17d5be2c373a46f599
Ikramboudia <ikramboudia303@gmail.com>
2022-03-10 11:28:00 +0100

    Qst2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,26 +1,31 @@
+package Classes;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
-	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
+import Interfaces.InterfaceDBConnection;
 
+public class DBConnection implements InterfaceDBConnection {
 	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+		static String BDD = "library_books";
+		static String url = "jdbc:mysql://localhost:3306/" + BDD;
+		static String user = "root";
+		static String passwd = "";
+	    private static Connection conn;
+	    private DBConnection() throws SQLException  {
 		}
-
-	    
-	    public Connection getConn() {
+		public static synchronized Connection getConn() throws SQLException {
+			if(conn == null)
+				try{
+					Class.forName("com.mysql.jdbc.Driver");
+					conn = DriverManager.getConnection(url,user, passwd);
+				}catch(Exception ex){
+					System.out.println(ex.getMessage());
+					System.out.println("couldn't connect!");
+					throw new RuntimeException(ex);
+				}
 			return conn;
 		}
 
-
-		
-	
 }
